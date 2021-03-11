86aa45a21ca153597607a10f9780cbc78e558fc7
Tony-Shnip <shnip12345@gmail.com>
2021-03-11 19:24:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let key = true;
+  str = str.split('');
+  while(key)
+  {
+    key = false;
+    for(let i = 1; i < str.length; i++)
+    {
+      for (let elem of bracketsConfig)
+      {
+        if(elem[0]==str[i-1] && elem[1]==str[i]){ str.splice(i-1, 2); i--; key = true; break;}
+      }
+    }
+  }
+  str = str.join('');
+  if(str !== '')return false;
+    else return true;
 }
